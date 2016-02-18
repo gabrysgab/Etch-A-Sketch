932126c569c60a9819bd4fe2193bc2d38676e265
Mateusz <mateuszgabrynowicz@gmail.com>
2016-02-18 21:09:56 +0100

    Added clear button, started function allowing to recreate grid

--- js/sketch.js
+++ js/sketch.js
@@ -9,20 +9,22 @@ var $square = $("<div />",{class : 'square'});
 
 $(document).ready(function(){
 
+Drawing(columns,rows);
 
 
-for (var i = 0; i < columns; i++) {
-	$row.append($square.clone());
-}
+//change of the square color by adding a new class with background-color 
+$(".square").hover(function(){
+	$(this).addClass("drawing");
+});
 
 
-for (var i = 0; i < rows; i++) {
-	$("#container").append($row.clone());
+$("#MyButton").click(function(){
+	NewBoard();
+});
 
 
-}
-
-$('.square').hover(
+/*change of the square color by using change of css value - background color
+ $('.square').hover(
 	function(){
 	$(this).css({"background-color":"black"});
 },
@@ -30,8 +32,43 @@ $('.square').hover(
 	$(this).css({"background-color":"black"});
 
 }
-);
+);*/
 
 //$(".row").css({"width":y});
 
 });
+
+function Drawing(columns,rows) {
+
+	var w = 600/columns + 'px';
+	var h = 600/rows + 'px';
+
+	$(".square").css({'width': w});
+	$(".square").css({'height': h});
+
+	//$(".row").css({'width': w});
+	$(".row").css({'height': h});
+
+	for (var i = 0; i < columns; i++) {
+	$row.append($square.clone());
+
+	
+}
+
+
+for (var i = 0; i < rows; i++) {
+	$("#container").append($row.clone());
+
+}
+}
+
+
+
+function NewBoard(){
+	$('#container div').remove();
+	var rows = prompt ("Please enter number of squares-vertical");
+	var columns = prompt ("Please enter number of squares-horizontal");
+	Drawing(columns,rows);
+
+
+}
