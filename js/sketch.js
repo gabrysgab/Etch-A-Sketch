1260c256c0fa1c1cd8930c1236ea57467ad11c65
Mateusz <mateuszgabrynowicz@gmail.com>
2016-02-22 21:48:03 +0100

    Added the option to create new custom grid

--- js/sketch.js
+++ js/sketch.js
@@ -1,21 +1,17 @@
-var rows = 30;
-var columns = 30;
+//var rows = 30;
+//var columns = 30;
 //var y = $(".square").outerWidth();
 //var height = y * rows;
-var $row = $("<div />", {class : 'row'});
-var $square = $("<div />",{class : 'square'});
+
 
 
 
 $(document).ready(function(){
 
-Drawing(columns,rows);
+Drawing(30,30);
 
 
 //change of the square color by adding a new class with background-color 
-$(".square").hover(function(){
-	$(this).addClass("drawing");
-});
 
 
 $("#MyButton").click(function(){
@@ -40,14 +36,22 @@ $("#MyButton").click(function(){
 
 function Drawing(columns,rows) {
 
-	var w = 600/columns + 'px';
-	var h = 600/rows + 'px';
+	var w = 600/columns;
+	var h = 600/rows;
 
-	$(".square").css({'width': w});
-	$(".square").css({'height': h});
+	var $row = $("<div />", {class : 'row'});
+	var $square = $("<div />",{class : 'square'});
+
+	$row.css({'height': h});
+				
+
+	$square.css({'height': h,
+					'width': w,
+					'display' : 'inline-block'});
+	
 
 	//$(".row").css({'width': w});
-	$(".row").css({'height': h});
+	
 
 	for (var i = 0; i < columns; i++) {
 	$row.append($square.clone());
@@ -60,15 +64,24 @@ for (var i = 0; i < rows; i++) {
 	$("#container").append($row.clone());
 
 }
+Draw();
 }
 
 
 
 function NewBoard(){
 	$('#container div').remove();
+	
+
 	var rows = prompt ("Please enter number of squares-vertical");
 	var columns = prompt ("Please enter number of squares-horizontal");
 	Drawing(columns,rows);
 
 
+}
+
+function Draw() {
+$(".square").hover(function(){
+	$(this).addClass("drawing");
+});
 }
